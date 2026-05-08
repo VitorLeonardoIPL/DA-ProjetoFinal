03c0d1465ab16beda8fb5f69f643094e0536281a
Diogo Almeida <astonrasecgames@gmail.com>
2026-05-08 22:38:57 +0100

    feat: add UserControls for dynamic view navigation
    Created UserControls for Dashboard, TipoArtigo, Artigo, Orcamento, and Estatistica. Added sidebar resource icons. Each UserControl can be swapped dynamically in the contentPanel of MainForm.
    
    Ultraworked with Sisyphus
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ ProjetoDA/View/Dashboard.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ProjetoDA.View
+{
+    partial class Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DASHBOARD";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.button1);
+            this.Name = "Dashboard";
+            this.Size = new System.Drawing.Size(1724, 753);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
